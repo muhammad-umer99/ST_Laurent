5ba5707b56f7952281d43221b3ebf08ed194f767
Muhammad Umer <147327053+muhammad-umer99@users.noreply.github.com>
2024-07-29 09:17:54 +0500

    Add files via upload

---
+++ Assets/JavaScript/script.js
@@ -0,0 +1,78 @@
+// Initialize Swiper
+document.addEventListener("DOMContentLoaded", function () {
+  var swiper = new Swiper(".swiper-container", {
+    loop: false,
+    slidesPerView: 1,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    simulateTouch: false, // Disable slide change using mouse drag or touch
+    allowTouchMove: false, // Disable touch-based slide changes
+    breakpoints: {
+      320: { slidesPerView: 1 },
+      480: { slidesPerView: 1 },
+      768: { slidesPerView: 1 },
+      1024: { slidesPerView: 1 },
+    },
+    on: {
+      transitionEnd: function () {
+        // Optional: Additional fixes or adjustments on slide transition end
+      },
+    },
+  });
+});
+
+var swiper = new Swiper(".swiper", {
+  slidesPerView: 2.5,
+  spaceBetween: 20,
+  sliderPerGroup: 2.5,
+  loop: true,
+  centerSlide: "true",
+  fade: "true",
+  grabCursor: "true",
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    520: {
+      slidesPerView: 2,
+    },
+    768: {
+      slidesPerView: 3,
+    },
+    1000: {
+      slidesPerView: 2.5,
+    },
+  },
+});
+
+function openTab(tabId) {
+  // Get all tab buttons and contents
+  const buttons = document.querySelectorAll(".tab-button");
+  const contents = document.querySelectorAll(".tab-content");
+
+  // Hide all contents and remove active class from all buttons
+  buttons.forEach((button) => button.classList.remove("active"));
+  contents.forEach((content) => content.classList.remove("active"));
+
+  // Show the clicked tab content and set its button as active
+  document.getElementById(tabId).classList.add("active");
+  document
+    .querySelector(`.tab-button[onclick="openTab('${tabId}')"]`)
+    .classList.add("active");
+}
+
+// Initialize with the first tab active
+document.addEventListener("DOMContentLoaded", () => {
+  openTab("tab1");
+});
