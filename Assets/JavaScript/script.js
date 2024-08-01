600159de07b0d1c04e0dfa1e71f96b6cc2b9eb68
muhammad-umer99 <mumer6221@gmail.com>
2024-08-01 14:43:22 +0500

    ad cicular motion text

--- Assets/JavaScript/script.js
+++ Assets/JavaScript/script.js
@@ -151,3 +151,11 @@ var swiper = new Swiper(".swiper-reviews", {
     },
   },
 });
+
+const text = document.querySelector(".text p");
+text.innerHTML = text.innerHTML
+  .split("")
+  .map(
+    (char, i) => `<span style="transform:rotate(${i * 7.2}deg)">${char}</span>`
+  )
+  .join("");
